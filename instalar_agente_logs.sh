e3684c14e621023f37385d9183c788c858afebc3
Alejandro Garcia <ale7batista@hotmail.com>
2025-05-13 08:28:04 +0000

    feat: script para instalar y configurar agente de logs

---
+++ instalar_agente_logs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y amazon-cloudwatch-agent
+
+cat <<EOF | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+{
+  "logs": {
+    "logs_collected": {
+      "files": {
+        "collect_list": [
+          {
+            "file_path": "/var/log/messages",
+            "log_group_name": "/aws/ec2/logs/messages",
+            "log_stream_name": "{instance_id}"
+          }
+        ]
+      }
+    }
+  }
+}
+EOF
+
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+  -a fetch-config \
+  -m ec2 \
+  -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json \
+  -s
+
